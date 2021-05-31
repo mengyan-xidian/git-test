7417e3da955d46ac73e36e2c8c2836b63075a18c
mengyan <yanmeng361@163.com>
2021-05-31 16:08:30 +0800

    first commit!

---
+++ src/main/java/com/atguigu/git/GitClass.java
@@ -0,0 +1,8 @@
+package com.atguigu.git;
+
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git");
+    }
+}
